19984377975dd6bf004b8be0a549d11c93a3d081
LeastDepressedDev <notsoqigan@gmail.com>
2026-03-25 16:11:22 +0300

    Last modification to previous feat

--- src/client/java/me/qigan/zacoxlo/mixin/client/MixinLivingEntity.java
+++ src/client/java/me/qigan/zacoxlo/mixin/client/MixinLivingEntity.java
@@ -3,10 +3,15 @@ package me.qigan.zacoxlo.mixin.client;
 import me.qigan.zacoxlo.Zacoxlo;
 import me.qigan.zacoxlo.cfg.Module;
 import net.minecraft.core.Holder;
+import net.minecraft.network.protocol.game.ClientboundAnimatePacket;
+import net.minecraft.server.level.ServerChunkCache;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffectUtil;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -35,7 +40,10 @@ public abstract class MixinLivingEntity {
 
     @Shadow
     public abstract MobEffectInstance getEffect(Holder<MobEffect> holder);
-    
+
+
+    @Shadow
+    public InteractionHand swingingArm;
 
     @Inject(method = "updateSwingTime", at = @At("HEAD"), cancellable = true)
     protected void updateSwingTime(CallbackInfo ci) {
@@ -62,6 +70,33 @@ public abstract class MixinLivingEntity {
             return 6 - (1 + MobEffectUtil.getDigSpeedAmplification((LivingEntity) (Object) this));
         } else {
             return this.hasEffect(MobEffects.MINING_FATIGUE) ? 6 + (1 + this.getEffect(MobEffects.MINING_FATIGUE).getAmplifier()) * 2 : 6;
+        }
+    }
+
+    /**
+     * @author LDD
+     * @reason Very unneeded shit. I could just do getCurrentSwingDuration override but that would interfere the swing animation packet... One worded - meh
+     */
+    @Overwrite
+    public void swing(InteractionHand interactionHand, boolean bl) {
+        int swgd = this.getCurrentSwingDuration();
+        if (!this.swinging || this.swingTime >= swgd / 2 || this.swingTime < 0) {
+            if (!Zacoxlo.MAIN_CFG.getBoolVal("hand_render") || (Zacoxlo.MAIN_CFG.getBoolVal("hand_render") &&
+                    !this.swinging || this.swingTime >= Math.round((float) swgd / Module.rtCfg.get("hand_render").get("Swing animation speed").getAsFloat()) / 2 || this.swingTime < 0
+            )) {
+                this.swingTime = -1;
+                this.swinging = true;
+                this.swingingArm = interactionHand;
+            }
+            if (((LivingEntity) (Object) this).level() instanceof ServerLevel) {
+                ClientboundAnimatePacket clientboundAnimatePacket = new ClientboundAnimatePacket((LivingEntity) (Object) this, interactionHand == InteractionHand.MAIN_HAND ? 0 : 3);
+                ServerChunkCache serverChunkCache = (ServerChunkCache) (((LivingEntity) (Object) this).level()).getChunkSource();
+                if (bl) {
+                    serverChunkCache.sendToTrackingPlayersAndSelf((LivingEntity) (Object) this, clientboundAnimatePacket);
+                } else {
+                    serverChunkCache.sendToTrackingPlayers((LivingEntity) (Object) this, clientboundAnimatePacket);
+                }
+            }
         }
     }
 }
